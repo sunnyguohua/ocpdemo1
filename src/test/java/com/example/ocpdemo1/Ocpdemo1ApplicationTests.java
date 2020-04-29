780c4282a88aa26292170b3d455ac209de31f93c
wangguohua <wangguohua@odianyun.com>
2020-04-29 15:11:32 +0800

    Initial commit

---
+++ src/test/java/com/example/ocpdemo1/Ocpdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ocpdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ocpdemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
